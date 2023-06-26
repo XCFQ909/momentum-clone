bf4efc6f23c5ec36bac9f8582062f6ddb9b70939
XCFQ909 <chrisnam03@naver.com>
2023-06-26 09:20:28 +0900

    does it still work?

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const loginForm = document.querySelector('#login-form')
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector('#greeting')
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+  }
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add('hidden')
+    loginForm.classList.add(HIDDEN_CLASSNAME)
     const username = loginInput.value;
-    console.log(username);
-    localStorage.setItem('username', username)
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove('hidden')
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit)
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
